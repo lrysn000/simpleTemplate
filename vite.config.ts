f7ee5340e20f37c335cbebef467e4349fa076da5
lrysn000 <shuozes@student.unimelb.edu.au>
2025-08-14 16:06:36 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/procurement-portal/' : '/', 
+  base: mode === 'production' ? '/simpleTemplate/' : '/', 
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
