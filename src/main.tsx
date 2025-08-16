aad7eaf841c897967bdb42089aeefe1bd5ed5d3d
lrysn000 <shuozes@student.unimelb.edu.au>
2025-08-17 01:44:41 +1000

    a

--- src/main.tsx
+++ src/main.tsx
@@ -4,26 +4,15 @@ import App from './App';
 import './index.css';
 
 import { BrowserRouter } from 'react-router-dom';
-import { PostHogProvider } from 'posthog-js/react';
-import posthog from 'posthog-js';
 
-posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
-  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-  debug: import.meta.env.MODE === 'development',
-});
-
-
-console.log("PostHog status:", posthog);
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <PostHogProvider client={posthog}>
-      <BrowserRouter
-        basename={import.meta.env.MODE === 'production' ? "/simpleTemplate" : "/"}
-      >
-        <App/>
-      </BrowserRouter>
-    </PostHogProvider>
+    <BrowserRouter
+      basename={ "/simpleTemplate/" }
+    >
+      <App/>
+    </BrowserRouter>
   </React.StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,12 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'production' ? '/simpleTemplate/' : '/', 
+  base: '/simpleTemplate/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-}));
+});
